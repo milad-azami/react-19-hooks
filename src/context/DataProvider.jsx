b09935f9d94509c0a7339add30dc19f3b1a6ebf4
Milad Azami <miladazami120@gmail.com>
2025-05-15 12:23:18 +0330

    Fix DataProvider component to correctly use DataContext.Provider

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -5,8 +5,8 @@ const data = "THIS IS A MOCK DATA";
 const DataContext = createContext();
 
 function DataProvider({ children }) {
-  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+  return <DataContext value={data}>{children}</DataContext>;
 }
 
 export { DataProvider, DataContext };
-// Commit message: "Add DataContext and DataProvider for global state management"
+
